db04fa87e48b00d00890b0fb64e588b5d9fcb88c
VuongGiang <vuonggiang.bkhn@gmail.com>
2018-07-06 19:48:07 +0700

    final

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
+             email: "giang@gmail.com",
              password:              "foobar",
              password_confirmation: "foobar",
                           admin: true,
